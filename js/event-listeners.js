2879e949822f148b85ae603d0fdba692a4bae5dc
Alex Van Houtven <38405710+Fenexito@users.noreply.github.com>
2025-07-29 11:28:41 -0600

    Update event-listeners.js

--- js/event-listeners.js
+++ js/event-listeners.js
@@ -100,7 +100,7 @@ function addFormAndFieldListeners() {
 
     dom.physicalCheckList1Select, dom.physicalCheckList2Select, dom.physicalCheckList3Select.forEach((select, index) => {
         select.addEventListener('change', () => {
-            const nextCheckbox = dom[`enablePhysicalCheck${index + 2}`;
+            const nextCheckbox = dom[`enablePhysicalCheck${index + 2}`];
             if (select.value !== '' && nextCheckbox && !nextCheckbox.checked) {
                 nextCheckbox.checked = true;
                 _updatePhysicalCheckListEnablement(dom.serviceSelect.value);
